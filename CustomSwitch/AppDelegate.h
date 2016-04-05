595548e75880523fd84800618a4b8425b2723f72
wuyt <594228924@qq.com>
2016-04-05 15:25:06 +0800

    Initial Commit

---
+++ CustomSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSwitch
+//
+//  Created by 吴玉铁 on 16/4/5.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSwitch
+//
+//  Created by 吴玉铁 on 16/4/5.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
